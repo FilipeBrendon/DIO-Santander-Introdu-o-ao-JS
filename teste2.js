c9bf5ee3bb63b90bf0e2f6e0eddd85c501f10f1a
Filipe Brendon <103082073+FilipeBrendon@users.noreply.github.com>
2022-07-28 17:28:50 -0300

    +

---
+++ teste2.js
@@ -0,0 +1,18 @@
+function returnEvenValues(array){
+    let evenNums=[];
+    let notEvenNums=[];
+    for(var i = 0; i < array.length; i++){
+        if(array[i] % 2 === 0){
+            evenNums.push(array[i])
+        }else{
+            notEvenNums.push(array[i])
+        }
+    }
+    console.log(evenNums)
+    console.log(notEvenNums)
+    
+}
+
+let array = [1, 2, 3, 4, 5, 6]
+
+returnEvenValues(array)
