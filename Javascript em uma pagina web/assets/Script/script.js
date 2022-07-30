4fc1fa9b664f1e1778fe75f1b16960e7e7cfda4a
Filipe Brendon <103082073+FilipeBrendon@users.noreply.github.com>
2022-07-29 23:58:38 -0300

    +

--- Javascript em uma pagina web/assets/Script/script.js
+++ Javascript em uma pagina web/assets/Script/script.js
@@ -1,1 +1,19 @@
-console.log("Hello World")
+let currentNumberWrapper = document.getElementById("currentNumber")
+let currentNumber = 0
+
+function decrement(){
+    currentNumber = currentNumber - 1
+    currentNumberWrapper.innerHTML=currentNumber
+    if(currentNumber < 0){
+        document.getElementById("currentNumber").style.color = "red";
+    }
+    
+}
+function increment(){
+    currentNumber = currentNumber + 1
+    currentNumberWrapper.innerHTML=currentNumber
+ 
+    if(currentNumber > 0){
+        document.getElementById("currentNumber").style.color = "blue";
+    }
+}
